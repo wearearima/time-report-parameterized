989c9a6efdb65c2c040c46488efd6c995466527a
jessica <jessica@arima.eu>
2020-06-19 11:05:14 +0200

    first commit

---
+++ src/main/java/eu/arima/tr/TimeReportsApplication.java
@@ -0,0 +1,14 @@
+package eu.arima.tr;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeReportsApplication.class, args);
+	}
+
+}
